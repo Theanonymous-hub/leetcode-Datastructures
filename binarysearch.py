af30a1d05d3d0574300c12fce3a4b61f46341afd
Prasana.R <76907455+Theanonymous-hub@users.noreply.github.com>
2022-03-08 21:24:30 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -14,3 +14,15 @@ class Solution:
             else:
                 l = mid + 1
         return -1
+
+    
+    #Example 1:
+
+#Input: nums = [-1,0,3,5,9,12], target = 9
+#Output: 4
+#Explanation: 9 exists in nums and its index is 4
+#Example 2:
+
+#Input: nums = [-1,0,3,5,9,12], target = 2
+#Output: -1
+#Explanation: 2 does not exist in nums so return -1
